0a8cbffc39c807bc829b7d0b448f9b13877aa9ef
luaraumc <Lu@140504>
2025-09-29 19:46:42 -0300

    feat: adicionar rotas para páginas principais e proteção de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom' // Routes: agrupa as rotas | Route: define uma rota
+import { RequireAuth, RequireAdmin } from './routes/ProtectedRoutes.jsx' // rotas protegidas
 
+// Páginas
 import CadastroUsuario from './pages/cadastro.jsx'
 import Login from './pages/login.jsx'
 import RecuperarSenha from './pages/recuperar-senha.jsx'
+import Home from './pages/home.jsx'
+import HomeUsuario from './pages/usuario/homeUsuario.jsx'
+import HomeAdmin from './pages/homeAdmin.jsx'
 
+// Componente principal da aplicação
 function App() {
-
   return (
     <>
     <Routes>
+    <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
         <Route path="/login" element={<Login />} />
         <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/homeUsuario" element={<HomeUsuario />} />
+        </Route>
+        <Route element={<RequireAdmin />}>
+          <Route path="/homeAdmin" element={<HomeAdmin />} />
+        </Route>
     </Routes>
     </>
   )
 }
-
 export default App
