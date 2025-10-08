b47a5d37bda40bc0c0b65e664deb9d1acfe8a1c3
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-10-08 12:26:46 -0300

    Refatorar o componente Vaga e atualize os links de navegação

--- src/pages/admin/vaga.jsx
+++ src/pages/admin/vaga.jsx
@@ -1,26 +1,44 @@
 import { Link, useNavigate } from "react-router-dom"; // criar links de navegação para redirecionar o usuário e voltar
 
-export default function Home() {
-	const navigate = useNavigate();
+// Página de cadastramento de vagas
+export default function Vaga() {
+
+	const navigate = useNavigate(); // navegação de páginas (voltar)
+
+	// HTML
 	return (
 		<div className="min-h-screen bg-slate-900 text-slate-200">
+
+			{/* HEADER */}
 			<header className="w-full border-b border-slate-800 bg-slate-950/80">
-				<div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
-					<Link to="/" className="text-xl font-semibold text-indigo-300 hover:text-indigo-200">
-						PFC
+				<div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+					<Link to="/homeAdmin" className="text-xl font-semibold text-indigo-300 hover:text-indigo-200">
+						PFC - Admin
 					</Link>
+					<button
+						onClick={logoutRedirecionar}
+						className="px-3 py-2 rounded-md border border-slate-700 text-slate-200 hover:bg-slate-800"
+					>
+						Sair
+					</button>
 				</div>
 			</header>
 
+			{/* CONTEÚDO PRINCIPAL */}
 			<main className="max-w-6xl mx-auto px-4 py-10">
-				{/* Ação: voltar à página anterior */}
-                <button
-                    onClick={() => navigate(-1)}
-                    className="mb-6 inline-flex items-center gap-2 px-3 py-2 rounded-md border border-slate-700 text-slate-200 hover:bg-slate-800"
-                >
-                    <span aria-hidden>←</span> Voltar
-                </button>
+
+				{/* BOTÃO VOLTAR */}
+				<button
+					onClick={() => navigate("/homeAdmin")}
+					className="mt-6 mb-6 inline-flex items-center gap-2 px-3 py-2 rounded-md border border-slate-700 text-slate-200 hover:bg-slate-800"
+				>
+					<span aria-hidden>←</span> Voltar
+				</button>
+
+				{/* título */}
 				<h1 className="text-3xl font-bold text-slate-100 text-center">Página de vagas</h1>
+
+				{/* descrição */}
 				<p className="mt-2 text-slate-300 text-center">Página de vagas</p>
 			</main>
 		</div>
