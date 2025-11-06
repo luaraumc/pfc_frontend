adddcb2192773c549f0c597db1057f99ed909be7
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-11-06 10:22:21 -0300

    Update API_URL to production environment

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import { useNavigate, Link } from "react-router-dom"; // navegação programáti
 import { authFetch, transformarJwt } from "../utils/auth"; // fetch autenticado com renovação automática de token
 import logoRumoTechno from "../../images/rumotechno-logo.svg";
 
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL ?? "https://pfcbackend-production-668a.up.railway.app";
 
 // Página de login
 export default function LoginUsuario() {
